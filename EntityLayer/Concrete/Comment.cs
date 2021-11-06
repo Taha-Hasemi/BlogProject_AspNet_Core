14cba73130b6fdafd76bbbeded8230cf9fa91b2c
Taha Haşemi <71307248+Taha-Hasemi@users.noreply.github.com>
2021-11-06 19:13:53 +0330

    Son Commit

--- EntityLayer/Concrete/Comment.cs
+++ EntityLayer/Concrete/Comment.cs
@@ -16,6 +16,7 @@ namespace EntityLayer.Concrete
         public string CommentContent { get; set; }
         public DateTime CommentDate { get; set; }
         public bool CommentStatus { get; set; }
+        public int CommentScore { get; set; }
 
 
         public int BlogID { get; set; }
